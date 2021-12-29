842f2f2ddc81e9134d99e25882f37ac6f9912c25
Lonna <ponyatov94@gmail.com>
2021-12-29 09:07:39 +0300

    new vers

--- sort_file.sh
+++ sort_file.sh
@@ -11,25 +11,14 @@ const_ext2="dat" #const extencion files which moveing
 
 cd $ROOT_DIR
 
-for each in * #name file - may be use regular expressions
-    do
-        extension="${each##*.}";
-        if [[ $extension == $const_ext1 || $extension == $const_ext2 ]]; then
-            date_file=$(date +%d-%m-%Y -r "$each");   #get data create file
-            ARRAY_DATES+=($date_file);
-            ARRAY_FILES+=($each);
-        fi
-    done
-
-DATES=$(printf "%s\n" "${ARRAY_DATES[@]}" | sort -u);
-
-for date in ${DATES[@]}; do
-    if [ ! -d "$date" ]; then #check for directory existence
-        mkdir "$date"
-    fi
-done
-
-for i in  ${ARRAY_FILES[@]}; do
-    dest=$(date +%d-%m-%Y -r "$i") 
-    mv $i $dest/$i
+for each in * #name file - may be use regular expressionsa
+do
+       extension="${each##*.}";
+       if [[ $extension == $const_ext1 || $extension == $const_ext2 ]]; then
+         date_file=$(date +%d-%m-%Y -r "$each");   #get data create file
+         if [ ! -d "$date_file" ]; then
+                mkdir "$date_file"
+         fi
+         mv $each $date_file/&each
+       fi
 done
